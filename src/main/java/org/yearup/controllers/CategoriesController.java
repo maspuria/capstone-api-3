45175d77653ae2b66088621dc140003634c0aea3
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-15 17:22:02 -0600

    reformat code in categories controller

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,75 +1,83 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 // add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
 @RestController
+// add the annotation to make this controller the endpoint for the following url
+// http://localhost:8080/categories
 @RequestMapping("/categories")
+// add annotation to allow cross site origin requests
 @CrossOrigin
 public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-
-    // create an Autowired controller to inject the categoryDao and ProductDao
-    @Autowired
-    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+    @Autowired  // create an Autowired controller to inject the categoryDao and ProductDao
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
+    {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
 
-    // add the appropriate annotation for a get action
-    @GetMapping
-    public List<Category> getAll() {
+    @GetMapping // add the appropriate annotation for a get action
+    public List<Category> getAll()
+    {
         return categoryDao.getAllCategories(); // find and return all categories
     }
 
-    // add the appropriate annotation for a get action
-    @GetMapping("/{id}")
-    public Category getById(@PathVariable int id) {
-        // get the category by id
-        return categoryDao.getById(id);
+    @GetMapping("/{id}") // add the appropriate annotation for a get action
+    public Category getById(@PathVariable int id)
+    {
+     return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId) {
-        // get a list of product by categoryId
-        return productDao.listByCategoryId(categoryId);
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        return productDao.listByCategoryId(categoryId);  // return all products that belong to this {categoryId}
     }
 
     // add annotation to call this method for a POST action
+    @PostMapping()
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        Category newCategory = categoryDao.create(category);
+        return newCategory;
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+
+    @PutMapping("{id}")
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id
+       categoryDao.update(id, category);
     }
 
-
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    @DeleteMapping("{id}")
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable int id)
     {
-        // delete the category by id
+     categoryDao.delete(id);
     }
 }
